c47341b1139d72070e8e2a0e00d21a2e291bd7ca
Carlos Nardo <c.cesar.nardo@gmail.com>
2025-01-17 17:35:46 -0300

    chore: add localStorage persistence
    (cherry picked from commit b4ca43cc1f40a5fc572d49c2482f1dce60e05f21)

--- src/components/layout/Sidebar.tsx
+++ src/components/layout/Sidebar.tsx
@@ -2,14 +2,19 @@
 
 import { FavoriteCharacter } from "@/components/characters/FavoriteCharacter";
 import { Menu } from "./Menu";
+import { useCharactersContext } from "@/contexts/Characters";
 
 export const Sidebar = () => {
+  const { favoriteCharacter } = useCharactersContext();
+
   return (
     <aside className="flex flex-col w-full lg:w-1/4 bg-gray-200">
       <nav>
-        <div className="p-4">
-          <FavoriteCharacter />
-        </div>
+        {favoriteCharacter && (
+          <div className="p-4">
+            <FavoriteCharacter />
+          </div>
+        )}
         <Menu />
       </nav>
     </aside>
