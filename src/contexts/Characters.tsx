eb0703b9006fe942ddf10f8c26f2fcdd047aab7c
Carlos Nardo <c.cesar.nardo@gmail.com>
2025-01-17 17:49:40 -0300

    fix: handle localStorage properly

--- src/contexts/Characters.tsx
+++ src/contexts/Characters.tsx
@@ -2,7 +2,13 @@
 
 import { getLocalStorage, setLocalStorage } from "@/services/localStorage";
 import { Character, House } from "@/types";
-import { createContext, PropsWithChildren, useContext, useState } from "react";
+import {
+  createContext,
+  PropsWithChildren,
+  useContext,
+  useEffect,
+  useState,
+} from "react";
 
 const houses: House[] = ["Gryffindor", "Slytherin", "Hufflepuff", "Ravenclaw"];
 
@@ -20,25 +26,31 @@ export const CharactersContext = createContext<
   CharactersContextType | undefined
 >(undefined);
 
-const storedPrefferedHouse = getLocalStorage("prefferedHouse");
-const storedFavoriteCharacter = getLocalStorage("favoriteCharacter");
-
-if (!storedPrefferedHouse) {
-  setLocalStorage("prefferedHouse", "Gryffindor");
-}
-
 export const CharactersProvider = ({ children }: PropsWithChildren) => {
   const [characters, setCharacters] = useState<Character[]>([]);
-  const [prefferedHouse, setPrefferedHouse] = useState<House>(
-    storedPrefferedHouse as House
-  );
+  const [prefferedHouse, setPrefferedHouse] = useState<House>("Gryffindor");
   const [favoriteCharacter, setFavoriteCharacter] = useState<string | null>(
-    storedFavoriteCharacter ?? null
+    null
   );
+
+  useEffect(() => {
+    const storedHouse = getLocalStorage("prefferedHouse") as House;
+    const storedFavoriteCharacter = getLocalStorage("favoriteCharacter");
+
+    if (storedHouse) {
+      setPrefferedHouse(storedHouse);
+    }
+    if (storedFavoriteCharacter) {
+      setFavoriteCharacter(storedFavoriteCharacter);
+    }
+  }, []);
+
+  useEffect(() => {
+    setLocalStorage("prefferedHouse", prefferedHouse);
+  }, [prefferedHouse]);
 
   const handleSetPrefferedHouse = (house: House) => {
     setPrefferedHouse(house);
-    setLocalStorage("prefferedHouse", house);
   };
 
   const handleSetFavoriteCharacter = (character: string | null) => {
